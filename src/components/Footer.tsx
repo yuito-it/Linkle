03515aa66d73fbe4ddb5c05d7029c2445c9cffac
YuitoAkatsuki <yuito@yuito.work>
2025-01-05 18:32:44 +0900

    背景色の変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,20 @@
-import { Box } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
+import theme from "@/theme";
 
 export default function Footer() {
     return (
-        <Box component='footer' flexGrow={0}>
-            <Box bgcolor='primary.main' p={2} textAlign='center'>
+        <ThemeProvider theme={theme}>
+            <Box component='footer' bgcolor='secondary.main' flexGrow={0}>
+                <Box p={2} textAlign='center'>
                 // left
-            </Box>
-            <Box bgcolor='primary.main' p={2} textAlign='center'>
+                </Box>
+                <Box p={2} textAlign='center'>
                 // center
-            </Box>
-            <Box bgcolor='primary.main' p={2} textAlign='center'>
+                </Box>
+                <Box p={2} textAlign='center'>
                 // right
+                </Box>
             </Box>
-        </Box>
+        </ThemeProvider>
     );
 }
