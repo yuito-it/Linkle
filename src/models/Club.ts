3ed53ab4eb1fa5539588763bdda9866ea4970622
YuitoAkatsuki <yuito@yuito.work>
2025-01-05 22:18:24 +0900

    Clubモデルを追加し、利用可能なプラットフォームを列挙する列挙型を定義

---
+++ src/models/Club.ts
@@ -0,0 +1,18 @@
+enum AvailableOn {
+    Chutobu = "Chutobu",
+    HighSchool = "HighSchool",
+}
+
+type Club = {
+    name: string;
+    slack_name: string;
+    slack_link: string;
+    available_on: AvailableOn;
+    short_description: string;
+    long_description: string;
+    created_at: string;
+    updated_at: string;
+    image: string;
+};
+
+export default Club;
