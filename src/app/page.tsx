132bc8ecf7a09355bb199b6ea108209a975afcba
YuitoAkatsuki <yuito@yuito.work>
2025-01-06 00:31:36 +0900

    ✨ ホームコンポーネントに検索クラブレスポンス型を追加し、MUIのBoxコンポーネントを導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import { SearchClubsResponse } from "@/libs/searchers/clubs";
+import { Box } from "@mui/material";
 
 export default function Home() {
+  const res: SearchClubsResponse = { status: "200", data: [] };
   return (
     <>
+      <Box sx={{ p: 2 }} >
+      </Box>
     </>);
 }
