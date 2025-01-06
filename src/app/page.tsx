5f7a644c67d8dd2631cb32d1ea750cbe629bbd9f
YuitoAkatsuki <yuito@yuito.work>
2025-01-06 16:46:32 +0900

    ✨  ホームページにClubSearchFormコンポーネントを統合し、レイアウトを改善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import ClubSearchForm from "@/components/SearchBox";
 import TitleLogo from "@/components/TitleLogo";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 export default function Home() {
   return (
     <>
-      <Box sx={{ p: 2 }} >
-        <TitleLogo/>
-      </Box>
+      <Stack sx={{ p: 2 }} justifyContent="center" alignItems="center">
+        <TitleLogo />
+        <Box width={2 / 5} sx={{ p: 5 }}>
+          <ClubSearchForm />
+        </Box>
+      </Stack>
     </>);
 }
