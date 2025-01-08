508e00324ec10bc2b152e56f8e4f23579602e495
YuitoAkatsuki <yuito@yuito.work>
2025-01-08 12:16:03 +0900

    💄  layout.tsxのGrid2コンポーネントの高さをminHeightに変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
-            <Grid2 bgcolor={'background.default'} height={'100vh'} sx={{
+            <Grid2 bgcolor={'background.default'} minHeight={'100vh'} sx={{
               '& ::-webkit-scrollbar': {
                 display: "none"
               },
