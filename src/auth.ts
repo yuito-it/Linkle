61653833082d62081673bb678fc3b6f44c1fcb7c
YuitoAkatsuki <yuito@yuito.work>
2025-01-12 12:16:23 +0900

    ✨ Google認証プロバイダーを追加し、NextAuthの設定を更新

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,14 @@
 import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [],
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    providers: [Google],
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account?.provider === "google") {
+                return profile?.email_verified && profile?.email?.endsWith("@nnn.ed.jp") ? true : false;
+            }
+            return true;
+        },
+    },
 })
