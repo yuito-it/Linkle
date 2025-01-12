993d7389dcfdd895c3147bb5d3868d061c1428c8
YuitoAkatsuki <yuito@yuito.work>
2025-01-12 14:53:02 +0900

    🔍  ホームページのメタデータを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,13 @@ import ClubSearchForm from "@/components/SearchBox";
 import TitleLogo from "@/components/TitleLogo";
 import { Box, Stack } from "@mui/material";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ホーム - Linkle",
+  description: "N/S高等学校同好会ポータルサイト「Linkle」です。自分の同好会のページをつくって宣伝したり、同好会を検索したりできます！",
+};
+
 export default function Home() {
   return (
     <>
