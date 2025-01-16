154794989cad9a74dd3ed97006eba97ab96191e0
YuitoAkatsuki <yuito@yuito.work>
2025-01-16 20:39:43 +0900

    :bug: サイドバーコンポーネントでユーザーのSlack名の取得方法を改善し、フォールバックを追加しました

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ export default async function Sidebar({ children }: Readonly<{ children: React.R
     const session = await auth();
     const user = await getCurrentUser(session?.user?.email ?? '');
     return (
-        <SidebarMain slack_name={user.slack_name}>
+        <SidebarMain slack_name={user?.slack_name ?? (session?.user?.name ?? session?.user?.email)}>
             {children}
         </SidebarMain>
     )
