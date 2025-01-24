05d2011d55ce29e132a36d01cd482ff1e417866d
yuito-it <yuito_it@outlook.jp>
2025-01-24 09:26:56 +0900

    :sparkles: 新しいクラブ作成APIエンドポイントを追加

---
+++ src/app/api/clubs/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const endpoint = process.env.DB_API_ENDPOINT;
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    const payload = {
+        name: body.name,
+        slack_name: body.slack_name,
+        slack_link: body.slack_link,
+        available_on: body.available_on,
+        visible: 0,
+        short_description: body.short_description,
+    }
+    console.log(payload);
+    const apiRes = await fetch(`${endpoint}/clubs`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+    });
+    if (apiRes.ok) {
+        return NextResponse.json({ status: 200 });;
+    } else {
+        console.error(apiRes.statusText);
+        console.error(apiRes.status);
+        console.error(await apiRes.text());
+        return NextResponse.error();
+    }
+}
