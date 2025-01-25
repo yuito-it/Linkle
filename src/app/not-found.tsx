c53a4cf2d55e4f50df2fadd09d9b29e03e976ad4
yuito-it <yuito@yuito.work>
2025-01-25 18:39:38 +0900

    ✨ 404および403エラーページを新規作成し、ユーザーに適切なメッセージを表示

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import { Link, Stack, Typography } from "@mui/material";
+
+export default async function forbidden() {
+    return (
+        <Stack spacing={2} alignItems="center" justifyContent="center" minHeight={"100vh"} textAlign="center">
+            <Typography variant="h1">404</Typography>
+            <Typography variant="h5">Not Found</Typography>
+            <Typography>
+                The page you are looking for does not exist.
+            </Typography>
+            <Link href="/">Go back to the HOME</Link>
+        </Stack>
+    );
+}
