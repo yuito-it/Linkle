c10e538f7e81c40d1e342674ae9dc83a345826c6
yuito-it <yuito@yuito.work>
2025-01-29 08:49:52 +0900

    💄 フッターコンポーネントのレイアウトを改善し、レスポンシブデザインを適用

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ import PoweredByVercel from 'powered-by-vercel'
 export default function Footer() {
     return (
         <ThemeProvider theme={theme}>
-            <Stack>
-                <Stack component='footer' direction={"row"} bgcolor='secondary.main' flexGrow={0}>
-                    <Box p={2} textAlign='center' width={1 / 3} sx={{ textAlign: 'left' }} margin={5}>
+            <Stack width={{ xs: "100vw", lg: (1 / 3) }}>
+                <Stack component='footer' direction={{ xs: "column", xl: "row" }} width={{ xs: "100vw", lg: (1 / 3) }} bgcolor='secondary.main' flexGrow={0} justifyContent={"center"} justifyItems={"center"}>
+                    <Box p={2} textAlign='left' width={{ xs: "100vw", lg: (1/3) }} margin={5}>
                         <Typography variant='h2' color='text.secondary'>
                             Linkle
                         </Typography>
@@ -34,7 +34,7 @@ export default function Footer() {
                             </Typography>
                         </Box>
                     </Box>
-                    <Box p={2} py={10} textAlign='left' width={1 / 3}>
+                    <Stack p={2} py={10} textAlign='left' width={{ xs: "100vw", lg: (1 / 3) }} justifyContent={"center"} justifyItems={"center"}>
                         <Typography variant='h5' color='text.secondary'>
                             免責事項
                         </Typography>
@@ -44,8 +44,8 @@ export default function Footer() {
                             このサイトを利用することで生じたいかなる損害についても、UniProject N/S Branchは一切の責任を負いません。<br />
                             また当団体は学外の団体の関連団体ですが、学外への情報提供は行っておりません。
                         </Typography>
-                    </Box>
-                    <Stack p={2} textAlign='left' width={1 / 3} justifyContent={"center"} alignItems={"center"} justifyItems={"center"} spacing={2}>
+                    </Stack>
+                    <Stack p={2} textAlign='left' width={{ xs: "100vw", lg: (1 / 3) }} justifyContent={"center"} alignItems={"center"} justifyItems={"center"} spacing={2}>
                         <iframe
                             src="https://uniproject.instatus.com/embed-status/3559d5ef/light-lg"
                             width="245"
@@ -61,31 +61,35 @@ export default function Footer() {
                         />
                     </Stack>
                 </Stack>
-                <Stack bgcolor='primary.light' p={2} direction={"row"} textAlign='center'>
+                <Stack bgcolor='primary.light' p={2} direction={{ xs: "column", xl: "row" }} textAlign='center' maxWidth={"100vw"}>
                     <Typography variant='body1' color='text.secondary'>
                         (c) 2025 UniProject All Rights Reserved.
                     </Typography>
-                    <Stack direction={"row"} flexGrow={1} justifyContent={"right"} justifyItems={"center"} spacing={2}>
-                        <Typography variant='body1' color='text.secondary'>
-                            <Link href='/tos'>
-                                &gt; 利用規約
-                            </Link>
-                        </Typography>
-                        <Typography variant='body1' color='text.secondary'>
-                            <Link href='/privacy'>
-                                &gt; プライバシーポリシー
-                            </Link>
-                        </Typography>
-                        <Typography variant='body1' color='text.secondary'>
-                            <Link href='/cookie'>
-                                &gt; Cookieポリシー
-                            </Link>
-                        </Typography>
-                        <Typography variant='body1' color='text.secondary'>
-                            <Link href='/about'>
-                                &gt; このサイトについて
-                            </Link>
-                        </Typography>
+                    <Stack direction={{ xs: "column", xl: "row" }} flexGrow={1} justifyContent={"right"} justifyItems={"center"} spacing={2} maxWidth={"100vw"}>
+                        <Stack direction={"row"} spacing={2} justifyContent={"center"} m={0} p={0} justifyItems={"center"}>
+                            <Typography variant='body1' color='text.secondary'>
+                                <Link href='/tos'>
+                                    &gt; 利用規約
+                                </Link>
+                            </Typography>
+                            <Typography variant='body1' color='text.secondary'>
+                                <Link href='/privacy'>
+                                    &gt; プライバシーポリシー
+                                </Link>
+                            </Typography>
+                        </Stack>
+                        <Stack direction={"row"} spacing={2} justifyContent={"center"} m={0} p={0} justifyItems={"center"}>
+                            <Typography variant='body1' color='text.secondary'>
+                                <Link href='/cookie'>
+                                    &gt; Cookieポリシー
+                                </Link>
+                            </Typography>
+                            <Typography variant='body1' color='text.secondary'>
+                                <Link href='/about'>
+                                    &gt; このサイトについて
+                                </Link>
+                            </Typography>
+                        </Stack>
                     </Stack>
                 </Stack>
             </Stack>
