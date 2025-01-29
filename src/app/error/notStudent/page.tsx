241335cf3a66c0370b5fa09fb8e853f5e421e8d4
yuito-it <yuito@yuito.work>
2025-01-29 16:03:29 +0900

    💄エラーページにテキスト中央揃えとパディングを追加

--- src/app/error/notStudent/page.tsx
+++ src/app/error/notStudent/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function SignOutPage() {
     return (
-        <Stack justifyContent={"center"} alignItems={"center"} spacing={2} justifyItems={"center"} height={"100vh"}>
+        <Stack justifyContent={"center"} alignItems={"center"} spacing={2} justifyItems={"center"} height={"100vh"} textAlign={"center"} p={{ xs: 2, xl: 0 }}>
             <Typography variant="h3">学園アカウントでログインしてください</Typography>
             <Typography variant="body1" textAlign={"center"}>Googleアカウントのチェックに失敗しました。@nnn.ed.jpや@n-jr.jpでログインしてください。<br />教職員の方の@nnn.ac.jpも使用可能です。</Typography>
             <Button href="/signin" variant="outlined">サインインページへ進む</Button>
