f32959d8c07312d3573324fda929c3ae65248e94
yuito-it <yuito@yuito.work>
2025-01-30 12:38:10 +0900

    :recycle: サイドバーコンポーネントから未使用のインポートを削除

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import { auth } from "@/auth";
 import SidebarMain from "./sidebar/main";
-import { Session } from "inspector/promises";
 import { SessionProvider } from "next-auth/react";
 
 export default async function Sidebar({ children }: Readonly<{ children: React.ReactNode }>) {
