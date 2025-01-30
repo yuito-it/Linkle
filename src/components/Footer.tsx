3bb62ed3e2b058eefd430b1e440b1ec60d478a86
yuito-it <yuito@yuito.work>
2025-01-30 14:56:13 +0900

    💄 フッターコンポーネントのレイアウトを改善し、テキストの最大幅を設定

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ import PoweredByVercel from 'powered-by-vercel'
 export default function Footer() {
     return (
         <ThemeProvider theme={theme}>
-            <Stack width={"100%"}>
-                <Stack component='footer' direction={{ xs: "column", xl: "row" }} width={"100%"} bgcolor='secondary.main' flexGrow={0} justifyContent={"center"} justifyItems={"center"}>
-                    <Box p={2} textAlign='left' width={{ xs: "100%", lg: (1 / 3) }} margin={{ xs: 0, xl: 5 }}>
+            <Stack width={"100%"} justifyContent={"center"} justifyItems={"center"}>
+                <Stack display={"flex"} direction={{ xs: "column", xl: "row" }} width={"100%"} bgcolor='secondary.main' flexGrow={0} justifyContent={"center"} justifyItems={"center"}>
+                    <Box p={2} textAlign='left' width={{ xs: "100%", xl: (1 / 3) }} margin={{ xs: 0, xl: 5 }} justifyContent={"center"} justifyItems={"center"}>
                         <Typography variant='h2' color='text.secondary'>
                             Linkle
                         </Typography>
@@ -34,18 +34,18 @@ export default function Footer() {
                             </Typography>
                         </Box>
                     </Box>
-                    <Stack p={2} py={{ xs: 0, xl: 10 }} textAlign='left' width={{ xs: "100%", lg: (1 / 3) }} justifyContent={"center"} justifyItems={"center"}>
-                        <Typography variant='h5' color='text.secondary'>
+                    <Box p={2} textAlign='left' width={{ xs: "100%", xl: (1 / 3) }} margin={{ xs: 0, xl: 5 }} justifyContent={"center"} justifyItems={"center"}>
+                        <Typography variant='h5' color='text.secondary' maxWidth={"275px"}>
                             免責事項
                         </Typography>
-                        <Typography variant='body1' color='text.secondary'>
+                        <Typography variant='body1' color='text.secondary' maxWidth={"450px"}>
                             このサイトは学園非公式です。<br />
                             このサイトに掲載されている情報は、誰でも編集できるため、その内容を保証するものではありません。<br />
                             このサイトを利用することで生じたいかなる損害についても、UniProject N/S Branchは一切の責任を負いません。<br />
                             また当団体は学外の団体の関連団体ですが、学外への情報提供は行っておりません。
                         </Typography>
-                    </Stack>
-                    <Stack p={2} textAlign='left' width={{ xs: "100%", lg: (1 / 3) }} justifyContent={"center"} alignItems={"center"} justifyItems={"center"} spacing={2}>
+                    </Box>
+                    <Box p={2} textAlign='left' width={{ xs: "100%", xl: (1 / 3) }} margin={{ xs: 0, xl: 5 }} justifyContent={"center"} justifyItems={"center"}>
                         <iframe
                             src="https://uniproject.instatus.com/embed-status/3559d5ef/light-lg"
                             width="245"
@@ -59,13 +59,13 @@ export default function Footer() {
                                 width: 245,
                             }}
                         />
-                    </Stack>
+                    </Box>
                 </Stack>
                 <Stack bgcolor='primary.light' p={2} direction={{ xs: "column", xl: "row" }} textAlign='center' maxWidth={"100%"}>
                     <Typography variant='body1' color='text.secondary'>
                         (c) 2025 UniProject All Rights Reserved.
                     </Typography>
-                    <Stack direction={{ xs: "column", xl: "row" }} flexGrow={1} justifyContent={"right"} justifyItems={"center"} spacing={2} maxWidth={"100%"}>
+                    <Stack direction={{ xs: "column", xl: "row" }} justifyContent={"right"} justifyItems={"center"} spacing={{ xs: 0, xl: 2 }} maxWidth={"100%"}>
                         <Stack direction={"row"} spacing={2} justifyContent={"center"} m={0} p={0} justifyItems={"center"}>
                             <Typography variant='body1' color='text.secondary'>
                                 <Link href='/tos'>
