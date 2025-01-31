38ce0457bc218e277896d958c584ed9fedfbb73b
yuito-it <yuito@yuito.work>
2025-01-31 13:29:41 +0900

    :fire: 不要な日付処理とログ出力を削除

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,15 +6,4 @@ export const config = {
 }
 
 export function middleware(request: NextRequest) {
-    const specificDate = new Date("2025-01-31T04:00:00Z");
-    const currentDate = new Date();
-    const jstOffset = 9 * 60;
-    const utcDate = new Date(currentDate.getTime() + (currentDate.getTimezoneOffset() * 60000));
-
-    console.log("JST Date: ", utcDate);
-    console.log("Specific Date: ", specificDate);
-    
-    if (utcDate < specificDate) {
-        return NextResponse.redirect(new URL("/pls-wait", request.url));
-    }
 }
