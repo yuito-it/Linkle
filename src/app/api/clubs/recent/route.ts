ad74f917462cefced73b5998e170dabd74e0e400
yuito-it <yuito@yuito.work>
2025-02-01 00:29:19 +0900

    :fix: クラブ取得APIのソート順を修正し、作成日で降順に変更

--- src/app/api/clubs/recent/route.ts
+++ src/app/api/clubs/recent/route.ts
@@ -7,8 +7,7 @@ const endpoint = process.env.DB_API_ENDPOINT;
 
 export const GET = async (req: NextRequest) => {
     const session = await auth();
-    console.log(`${endpoint}/clubs?size=8&order=created_at&filter1=visible,eq,${session ? 0x1 | 0x2 : 0x2}`);
-    const apiRes = await fetch(`${endpoint}/clubs?size=8&order=created_at&filter1=visible,ge,${session ? 0x1 : 0x2}`);
+    const apiRes = await fetch(`${endpoint}/clubs?size=8&order=created_at,desc&filter1=visible,ge,${session ? 0x1 : 0x2}`);
     if (apiRes.ok) {
         const data = await apiRes.json();
         return NextResponse.json(data.records);
