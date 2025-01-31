c15731701bbcbabba24e11da22f28e1b789cc6d3
yuito-it <yuito@yuito.work>
2025-02-01 08:59:20 +0900

    :recycle: クラブ取得APIにPATCHメソッドを追加

--- src/app/api/clubs/[id]/route.ts
+++ src/app/api/clubs/[id]/route.ts
@@ -30,3 +30,16 @@ export async function PUT(request: Request, { params }: { params: Promise<{ id: 
     });
     return Response.json({ status: apiRes.status });
 }
+
+export async function PATCH(request: Request, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    const body = await request.json();
+    const apiRes = await fetch(`${endpoint}/clubs/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+    return Response.json({ status: apiRes.status });
+}
