a3749c12c31b99d58d4cbb1b41c6f090b70c90a0
yuito-it <yuito@yuito.work>
2025-02-06 10:20:08 +0900

    :bug: APIエンドポイントの構築を改善し、リクエストヘッダーにセッションIDを追加

--- src/lib/club.ts
+++ src/lib/club.ts
@@ -1,14 +1,22 @@
-import { auth } from "@/auth";
 import Club from "@/models/Club";
 import { headers } from "next/headers";
-
-const apiBase = process.env.DB_API_ENDPOINT;
 
 export type fetchErrorResponse = "notfound" | "forbidden";
 
 export const getClubById = async (id: string): Promise<Club | fetchErrorResponse> => {
   try {
-    const res = await fetch(`${apiBase}/clubs/${id}`);
+    const headersData = await headers();
+    const host = headersData.get("host");
+    const protocol =
+      headersData.get("x-forwarded-proto") ?? host?.startsWith("localhost") ? "http" : "https";
+    const cookie = headersData.get("cookie");
+    const sessionID = cookie?.split(";").find((c) => c.trim().startsWith("authjs.session-token"));
+    const apiBase = `${protocol}://${host}`;
+    const res = await fetch(`${apiBase}/api/clubs/${id}`, {
+      headers: new Headers({
+        cookie: sessionID ?? "",
+      }),
+    });
     if (res.status == 403) return "forbidden";
     const club = (await res.json()) as Club;
     if (!club) return "notfound";
