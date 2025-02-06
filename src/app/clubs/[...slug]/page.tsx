a6bf3300f707f86274482fd3d0c2938e7c67e173
yuito-it <yuito@yuito.work>
2025-02-06 10:20:12 +0900

    :art: 不要なメタデータ生成関数を削除し、コードを簡素化

--- src/app/clubs/[...slug]/page.tsx
+++ src/app/clubs/[...slug]/page.tsx
@@ -5,64 +5,8 @@ import EditClub from "@/components/clubPage/edit";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 
-import { headers } from "next/headers";
-
-import { Metadata } from "next";
 import { Suspense } from "react";
 import { CircularProgress, Stack, Typography } from "@mui/material";
-
-/*export const generateMetadata = async ({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}): Promise<Metadata> => {
-  const headersData = await headers();
-  const host = headersData.get("host");
-  const protocol =
-    (headersData.get("x-forwarded-proto") ?? host?.startsWith("localhost")) ? "http" : "https";
-  const apiBase = `${protocol}://${host}`;
-  const { slug } = await params;
-  const res = await fetch(`${apiBase}/api/clubs/${slug[0]}`);
-  if (res.status === 200) {
-    const data = await res.json();
-    const imageFinalURL = data.image ? (await fetch(data.image)).url : undefined;
-
-    return {
-      title: `${data.name} - Linkle`,
-      description: `${data.short_description}`,
-      openGraph: {
-        title: `${data.name} - Linkle`,
-        description: `${data.short_description}`,
-        type: "website",
-        url: `${apiBase}/clubs/${slug[0]}`,
-        images: imageFinalURL ?? undefined,
-        siteName: "同好会ポータル Linkle",
-      },
-      twitter: {
-        card: "summary_large_image",
-        site: "@UniPro_digital",
-        title: `${data.name} - Linkle`,
-        description: `${data.short_description}`,
-      },
-    };
-  } else if (res.status === 404) {
-    return {
-      title: "404 - Not Found - Linkle",
-      description: "404 - Not Found",
-    };
-  } else if (res.status === 403) {
-    console.log("403");
-    return {
-      title: "403 - Unauthorized - Linkle",
-      description: "403 - Unauthorized",
-    };
-  } else {
-    return {
-      title: "500 - Internal Server Error - Linkle",
-      description: "500 - Internal Server Error",
-    };
-  }
-};*/
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const slug = (await params).slug;
