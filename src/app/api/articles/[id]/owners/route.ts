193ed4b14d64f7278b25f72169e146258224f470
yuito-it <yuito@yuito.work>
2025-02-19 14:48:43 +0900

    :sparkles: 記事のオーナーに関連するクラブを取得するためのGETエンドポイントを追加

---
+++ src/app/api/articles/[id]/owners/route.ts
@@ -0,0 +1,9 @@
+const endpoint = process.env.DB_API_ENDPOINT;
+
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const user_clubRes = await fetch(`${endpoint}/article_managers/?filter1=club,eq,${id}`);
+  const res = (await user_clubRes.json()) as { records: [{ club: number }] };
+  const user_clubData = res.records.map((record) => record.club);
+  return Response.json(user_clubData);
+}
