45d306e24b46bac02e3b2fb54923db3eed27d8d7
yuito-it <yuito@yuito.work>
2025-02-19 14:46:21 +0900

    🏷️ Articleモデルを新規作成し、記事の構造を定義

---
+++ src/models/Article.ts
@@ -0,0 +1,14 @@
+type Article = {
+  id: number;
+  title: string;
+  authors: string[] | undefined;
+  clubs: number[] | undefined;
+  description: string;
+  created_at: string;
+  updated_at: string;
+  image: string;
+  image_file: string | undefined;
+  visible: number;
+};
+
+export default Article;
